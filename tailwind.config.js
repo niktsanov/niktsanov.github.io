19d56dcada6ed366f35a2e716ae2089c468dd1d5
Nikolay Tsanov <nik.tzanov@gmail.com>
2020-05-05 17:12:47 +0100

    Small improvements to the font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'body': ['Work Sans', 'sans-serif'],
+        'sans': ["Work Sans", "system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "Noto Sans", "sans-serif", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
       },
       colors: {
         blue: {
